f456925771d8186b910e12965bfb33ae398d9279
Shaik Mohammad Ashraf <ashrafskmd8@gmail.com>
2026-05-19 15:14:03 +0530

    Complete futuristic portfolio upgrade

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-console.log("Portfolio Loaded Successfully");
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+});
+
+const texts = [
+    "AI & Data Science Student",
+    "Frontend Developer",
+    "Machine Learning Enthusiast",
+    "Future AI Engineer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+        setTimeout(type,1500);
+    }
+    else{
+        setTimeout(type,100);
+    }
+
+})();
+
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+
+    const reveals = document.querySelectorAll(".reveal");
+
+    for(let i = 0; i < reveals.length; i++){
+
+        const windowHeight = window.innerHeight;
+        const revealTop = reveals[i].getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add("active");
+        }
+
+    }
+
+}
